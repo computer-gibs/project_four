fd3d212031d643e1a7ffea1fd1c51bb34e6985b0
AlexanderK <alexandr.nikitin2404@gmail.com>
2023-01-12 21:59:53 +0500

    app tests check

--- TestingTheAI.py
+++ TestingTheAI.py
@@ -1,18 +1,18 @@
-import gradio as gr
-import torch
+import LaunchScript
 from PIL import Image
-from torchvision import transforms
-from diffusers import StableDiffusionImageVariationPipeline
+import pytest
 
-#Проверка на размерность входящего изображения
+#Проверка на допустимость размерности входящего изображения больше 1000х1000 пикселей
 def test_size():
+    with pytest.raises(ValueError):
+        LaunchScript.main(input_image=Image.open(url="https://testit.software/storage/app/media/intro-testit-pro__1390.jpg"))
 
 #Проверка на формат входящего файла
 def test_format():
+    with pytest.raises(ValueError):
+        LaunchScript.main(input_image=Image.open(url="https://www.africau.edu/images/default/sample.pdf"))
 
 #Проверка на картинку с прозрачным фоном
 def test_alpha():
-
-#Проверка на потенциально небезопасный контент
-def test_unsafe():
+    LaunchScript.main(input_image=Image.open(url="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"))
 
