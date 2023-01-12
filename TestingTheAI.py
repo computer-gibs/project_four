b62ecef62df43c3dd23fe1055e6fd8e9d5eadb9f
AlexanderK <alexandr.nikitin2404@gmail.com>
2023-01-12 21:45:51 +0500

    tests template

---
+++ TestingTheAI.py
@@ -0,0 +1,18 @@
+import gradio as gr
+import torch
+from PIL import Image
+from torchvision import transforms
+from diffusers import StableDiffusionImageVariationPipeline
+
+#Проверка на размерность входящего изображения
+def test_size():
+
+#Проверка на формат входящего файла
+def test_format():
+
+#Проверка на картинку с прозрачным фоном
+def test_alpha():
+
+#Проверка на потенциально небезопасный контент
+def test_unsafe():
+
